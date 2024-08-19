b7c434e6c4eb4993c60e700e9dd84e45c4eadfc8
Joe King <joeking92596@gmail.com>
2024-08-19 14:36:58 -0500

    Initial Commit

---
+++ build/CMakeFiles/Stirling.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stirling.dir/src/main.cpp.o"
+  "CMakeFiles/Stirling.dir/src/main.cpp.o.d"
+  "Stirling"
+  "Stirling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stirling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
